a933b0feeccb45764765e88c20d6838653bec2da
Andy Cobley <acobley@computing.dundee.ac.uk>
2015-01-22 14:41:49 +0000

    initial

---
+++ SensorSync/src/main/java/uk/ac/dundee/computing/aec/sensorsync/SensorSync.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uk.ac.dundee.computing.aec.sensorsync;
+
+/**
+ *
+ * @author andycobley
+ */
+public class SensorSync {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

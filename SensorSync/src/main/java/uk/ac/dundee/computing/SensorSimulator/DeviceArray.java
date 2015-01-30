e84f7372ab2d2c094ba16524dab77e5ad7ec5521
Andy Cobley <acobley@computing.dundee.ac.uk>
2015-01-30 15:05:37 +0000

    json encoding

--- SensorSync/src/main/java/uk/ac/dundee/computing/SensorSimulator/DeviceArray.java
+++ SensorSync/src/main/java/uk/ac/dundee/computing/SensorSimulator/DeviceArray.java
@@ -5,6 +5,9 @@
  */
 package uk.ac.dundee.computing.SensorSimulator;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  *
  * @author Administrator
@@ -25,23 +28,41 @@ public class DeviceArray {
         System.out.println("DeviceName"+dd.getDevice());
         System.out.println("Insertion_time"+dd.getInsertion_time());
         Sensor sensors[]=dd.getSensors();
+        JSONArray jsonSensors = new JSONArray();
+        JSONObject Record=null;
         for (int i=0; i<sensors.length;i++){
             int type=sensors[i].getType();
             Object value=sensors[i].getValue();
+            Record = new JSONObject();
+            Record.put("name",sensors[i].getName());
             switch (type){
                 case 1:
                     Float fValue=(Float)value;
                     System.out.println(sensors[i].getName()+":"+fValue);
+                    
+                    Record.put("fValue",fValue);
                     break;
                 case 2:
                     Integer iValue=(Integer)value;
                     System.out.println(sensors[i].getName()+":"+iValue);
+                       Record.put("iValue",iValue);
                     break;
                 default:
                     System.out.println("Type"+type);
                     break;
             }
+             jsonSensors.put(Record);
         }
+       
+        JSONObject jsonDevice= new JSONObject();
+        jsonDevice.put("device", dd.getDevice());
+        jsonDevice.put("Insertion_time",dd.getInsertion_time());
+        JSONObject json=new JSONObject();
+         json.put("sensors", jsonSensors);
+        json.put("SensorData", jsonDevice);
+       
+        
+        System.out.println(json.toString());
     }
 
 }
